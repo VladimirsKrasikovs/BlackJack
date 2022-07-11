9b610ee9f8a010d74597cf6515583a3fe852ca42
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-07-11 18:46:48 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,14 +7,15 @@ public class Game {
         int cardName = card.getCard();
         // FIRST CARD
         System.out.printf("Your first card is " + cardName + ". ");
-        Cards secondCard = new Cards();
+//        Cards secondCard = new Cards();
         // SECOND CARD
-        int cardName2 = secondCard.getCard();
+        int cardName2 = card.getCard();
         System.out.println("Your second card is " + cardName2);
         System.out.println("TOTAL SCORE IS " + (cardName2 + cardName));
         int firstTotal = (cardName2 + cardName);
         if (firstTotal == 21) {
             System.out.println("BLACK JACK !!! YOU WIN!");
+            return
         }
 
         Scanner scan = new Scanner(System.in);
@@ -27,22 +28,22 @@ public class Game {
                 System.out.println("Your third card is " + cardName3);
                 System.out.println("TOTAL SCORE IS " + (cardName3 + cardName2 + cardName));
                 int totalPlayer = (cardName3 + cardName2 + cardName);
-                if ((totalPlayer) > 21) {
+                if (totalPlayer > 21) {
                     System.out.println("Sorry, Dealer Wins");
-                } else if ((totalPlayer == 21)) {
+                } else if (totalPlayer == 21) {
                     System.out.println("BLACK JACK You win!!");
                     break;
                 }
                 // STAND AFTER 3 CARD
-                Scanner scan2 = new Scanner(System.in);
-                System.out.println("Do you want to 1.HIT or 2.STAND FOUR");
+//                Scanner scan2 = new Scanner(System.in);
+//                System.out.println("Do you want to 1.HIT or 2.STAND FOUR");
                 int player2 = scan.nextInt();
                 switch (player2) {
                     case 1:
                         int cardName4 = card.getCard();
-                        System.out.println("Youre four card is " + cardName4);
+                        System.out.println("Your four card is " + cardName4);
                         int totalPLayer1 = totalPlayer + cardName4;
-                        System.out.println("TOTAL SCORE IS " + (totalPLayer1));
+                        System.out.println("TOTAL SCORE IS " + totalPLayer1);
                         if (totalPLayer1 > 21) {
                             System.out.println("Sorry, Dealer wins");
                             break;
@@ -51,7 +52,7 @@ public class Game {
                         System.out.println("Now Dealer in game");
                         Dealer dealer4 = new Dealer();
                         int dealerCard1 = dealer4.dealerGetCard();
-                        System.out.printf("Dealer first card is " + dealerCard1 + ".");
+                        System.out.print("Dealer first card is " + dealerCard1 + ".");
                         Dealer dealer5 = new Dealer();
                         int dealerCard2 = dealer5.dealerGetCard();
                         System.out.println("Dealer second card is " + dealerCard2);
@@ -65,7 +66,7 @@ public class Game {
                 System.out.println("Now Dealer in game");
                 Dealer dealer = new Dealer();
                 int dealerCard1 = dealer.dealerGetCard();
-                System.out.printf("Dealer first card is " + dealerCard1 + ".");
+                System.out.print("Dealer first card is " + dealerCard1 + ".");
                 Dealer dealer2 = new Dealer();
                 // Dealer get 2 card
                 int dealerCard2 = dealer.dealerGetCard();
@@ -73,11 +74,10 @@ public class Game {
                 int dealerTotal = (dealerCard1 + dealerCard2);
                 System.out.println("TOTAL SCORE is " + (dealerTotal));
                 //if BLack Jack dealer win
-                if ((dealerTotal) == 21) {
+                if (dealerTotal == 21) {
                     System.out.println("Black Jack Dealer win!");
                 }
-                //IF dealer is score is less than 18
-                else if ((dealerTotal <= 18)) {
+                else if (dealerTotal <= 18) {
                     int dealerCard3 = dealer.dealerHit();
                     System.out.println("Dealer third card is " + dealerCard3);
                     int dealerTotal2 = dealerTotal + dealerCard3;
@@ -89,20 +89,17 @@ public class Game {
                     } else if (dealerTotal2 < firstTotal) {
                         System.out.println("Congratulations , you won. Your score is " + firstTotal + " and dealer score is " + dealerTotal2);
                     }
-                    //IF dealer score is 19!
-                } else if ((dealerTotal == 19)) {
+                } else if (dealerTotal == 19) {
                     if (dealerTotal >= firstTotal) {
                         System.out.println("Sorry Dealer win. Your score is " + firstTotal + " and dealer score is " + dealerTotal);
                     } else if (dealerTotal < firstTotal) {
                         System.out.println("Congratulations , you won. Your score is " + firstTotal + " and dealer score is " + dealerTotal);
                     }
-                    //If dealer score is 20!
-                } else if ((dealerTotal == 20)) {
+                } else if (dealerTotal == 20) {
                     if (dealerTotal >= firstTotal) {
                         System.out.println("Sorry Dealer win. Your score is " + firstTotal + " and dealer score is " + dealerTotal);
                     } else if (dealerTotal < firstTotal) {
                         System.out.println("Congratulations , you won. Your score is " + firstTotal + " and dealer score is " + dealerTotal);
-                        Sout
                     }
                 }
         }
@@ -111,6 +108,12 @@ public class Game {
 
 class Cards {
     public int getCard() {
+//        MIN = 5;
+//        MAX = 12;
+//
+//        int v = MIN + Math.random() * (MAX-MIN);
+//        5 + (random(7))
+//
         int value = (int) (Math.random() * 12);
 
         return value + 2;
