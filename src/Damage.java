e772afb927d9e6c52f67762e20dc09ba5fad3625
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-07-14 07:18:28 +0300

    Create Damage.java

---
+++ src/Damage.java
@@ -0,0 +1,16 @@
+public class Damage {
+    private int min = 10;
+    private int max = 18;
+    public Damage(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+}
